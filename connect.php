4ddf0a7552cf5c20b8e0fbfbf46434ac481ed108
rinish337 <58217627+rinish337@users.noreply.github.com>
2019-12-04 14:57:45 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost"; 
-$username = "root";
-$password = "";
-
-
-    $conn = new PDO("mysql:host=$servername;", $username, $password,);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
